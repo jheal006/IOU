4094b921f96f9ba835f4549c3175b7653a7d8f8e
jheal006 <jheal006@gmail.com>
2018-06-22 19:33:57 -0700

    Form adjustments

--- js/app.js
+++ js/app.js
@@ -1,15 +1,58 @@
-on window load
 
+window.onload = function () {
 
-form.on(submit){
-	
-	event.preventDefult();
-	
-	form.grabvalues 
-	
-	iterate and clump them together into var data
-	
-	insertTransaction(data);
-	
-	window.location.reload
-}
+// Stop the form from submitting when a button is pressed
+// form.addEventListener('submit', function(e) {
+$( "form" ).on( "submit", function( event ) {
+	console.log("WAT HAPPENED?!?");
+	event.preventDefault();
+  // Retrieve Friends
+	var name = $(this[name='friend1']).val();
+  //Create Array of Friends
+  // friendList.push(name);
+	// console.log("SHOULD OUTPUT Array===", friendList);
+// 	iterate and clump them together into var data
+//
+// 	insertUser(data);
+	db.transaction(function (tx) {
+		// tx.executeSql('DROP TABLE IF EXISTS FRIENDS');
+	 	tx.executeSql('CREATE TABLE IF NOT EXISTS FRIENDS (id INTEGER PRIMARY KEY, name)');
+	 	msg = '<p>Log message create and row instered.</p>';
+		// tx.executeSql('INSERT INTO FRIENDS (name) VALUES ("foobar")');
+		// tx.executeSql('INSERT INTO FRIENDS (name) VALUES ("logmsg")');
+		tx.executeSql('INSERT INTO FRIENDS (name) VALUES (?)', [name]);
+	});
+// 	window.location.reload
+	// window.location.href='app.html';
+
+});
+
+$("#buttonTest").on('click', function() {
+  	db.transaction(function (tx) {
+  		 tx.executeSql('SELECT * FROM FRIENDS', [], function (tx, results) {
+  				var len = results.rows.length, i;
+  				msg = "<p>Found rows: " + len + "</p>";
+  				document.querySelector('#test').innerHTML +=  msg;
+
+  				for (i = 0; i < len; i++) {
+  					 msg = "<p><b>" + results.rows.item(i).name + "</b></p>";
+  					 // msg = "<p><b>" + results.rows.item(i).id + "</b></p>"
+  					 document.querySelector('#test').innerHTML +=  msg;
+  				}
+  		 }, null);
+  	});
+  });
+
+  $("#next").on('click', function() {
+    console.log('test');
+    window.location.href="app.html";
+    populateDropdown();
+  });
+
+  $("#cleardb").on('click', function() {
+    	db.transaction(function (tx) {
+    		 tx.executeSql('DROP TABLE IF EXISTS FRIENDS');
+      });
+    });
+
+};
--- js/init.js
+++ js/init.js
@@ -1,59 +1,5 @@
-window.onload = function () {
 
 // Open DB Connection
 var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
 var msg;
-db.transaction(function (tx) {
-	 tx.executeSql('DROP TABLE IF EXISTS FRIENDS');
-	 tx.executeSql('CREATE TABLE IF NOT EXISTS FRIENDS (id, name)');
-	 msg = '<p>Log message create and row instered.</p>';
-});
 console.log(db);
-
-
-
-// Stop the form from submitting when a button is pressed
-// form.addEventListener('submit', function(e) {
-$( "form" ).on( "submit", function( event ) {
-	console.log("WAT HAPPENED?!?")
-	// form.on(submit) {
-	event.preventDefault();
-//
-// 	form.grabvalues
-	var name = $(this[name='friend1']).val();
-	console.log("SHOULD OUTPUT NAME===", name);
-// 	iterate and clump them together into var data
-//
-// 	insertUser(data);
-	db.transaction(function (tx) {
-		tx.executeSql('INSERT INTO FRIENDS (id, name) VALUES (NULL, "foobar")');
-		tx.executeSql('INSERT INTO FRIENDS (id, name) VALUES (NULL, "logmsg")');
-		tx.executeSql('INSERT INTO FRIENDS (id, name) VALUES (NULL, ?)', [name]);
-	});
-
-
-// 	window.location.reload
-// }
-});
-
-$("#buttonTest").on('click', function() {
-	db.transaction(function (tx) {
-		 tx.executeSql('SELECT * FROM FRIENDS', [], function (tx, results) {
-				var len = results.rows.length, i;
-				msg = "<p>Found rows: " + len + "</p>";
-				document.querySelector('#test').innerHTML +=  msg;
-
-				for (i = 0; i < len; i++) {
-					 msg = "<p><b>" + results.rows.item(i).name + "</b></p>";
-					 // msg = "<p><b>" + results.rows.item(i).id + "</b></p>"
-					 document.querySelector('#test').innerHTML +=  msg;
-				}
-		 }, null);
-	});
-});
-
-
-
-
-
-};
